3d5aa8683fa6329191f4b37e2aa13cfe845e6939
soham560 <77228631+soham560@users.noreply.github.com>
2021-04-27 11:04:20 +0530

    Add files via upload

---
+++ Stone.js
@@ -0,0 +1,29 @@
+class stone {
+    constructor(x, y,width,height) {
+      var options = {
+        'density':0.8,
+        'friction': 0.9,
+        'restitution':12,
+      };
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+     
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('white')
+      fill('orange')
+      
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
+  
